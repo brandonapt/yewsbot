376bf164de3b7f1ceb05f9593adb44d8d866a179
brandoge91 <brandoge91@gmail.com>
2024-02-25 20:07:58 -0500

    add google analytics

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-gtag'],
+  gtag: {
+    id: 'G-TF5W9DN30P'
+  }
 })
